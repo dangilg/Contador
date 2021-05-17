32e1840b5ce39403a316b504f0684f4fb3984949
danie <danie@DESKTOP-IM7UE41>
2021-05-17 21:10:08 +0200

    Inicio del programa

---
+++ Contador/src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import stateMachine.*;
+
+/**
+ *
+ * @author danie
+ */
+public class Main {
+    public static StateMachine stateMachine;
+    
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        stateMachine = new StateMachine();
+    }
+}
